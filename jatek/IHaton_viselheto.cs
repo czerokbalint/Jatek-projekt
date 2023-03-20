39a2178bbb441464b498595a08465a61011485b3
czerokbalint <83694291+czerokbalint@users.noreply.github.com>
2023-03-20 10:08:24 +0100

    Add files via upload

---
+++ jatek/IHaton_viselheto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jatek
+{
+    interface IHaton_viselheto : Ilathato
+    {
+        int tarolsz_x { get; }
+        int tarolsz_xx { get; }
+        int tarolsz_xxx { get; }
+
+    }
+}
---
+++ jatek/ITalalati_arany.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jatek
+{
+    interface ITalalati_arany
+    {
+        int talalat { get; }
+    }
+}
