8400b5ae245c4592e67ec125b2de977f724e58ea
kassaibence1 <97015143+kassaibence1@users.noreply.github.com>
2023-05-24 12:28:06 +0200

    Update IViselheto.cs

--- jatek/IViselheto.cs
+++ jatek/IViselheto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,7 @@ namespace jatek
     interface IViselheto : Ilathato
     {
         int armor { get; }
-       
+        
                 
             
     }
